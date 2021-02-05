e351adac758ae188025a0439777e6bc82822afb7
gittestsite1 <gittestsite1@gmail.com>
2021-02-05 19:51:01 +0900

    테스트 코드 수정

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,16 +1,23 @@
 package hello.core.member;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
+    MemberService memberService = new MemberServiceImpl();
     @Test
     void join(){
 
         // given
-        // when
-        // then
+        Member member = new Member(1L, "MemberA", Grade.VIP);
 
+        // when
+        memberService.save(member);
+        Member findMember = memberService.findByid(1L);
+
+        // then
+        Assertions.assertThat(member).isEqualTo(findMember);
 
         //
     }
